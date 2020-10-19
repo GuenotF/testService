b0879b35a6650993d266e5b57733bb4c7255ef84
GuenotF <37656401+GuenotF@users.noreply.github.com>
2020-10-19 13:10:44 +0200

    Add files via upload

---
+++ MyService/Service1.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace MyService
+{
+    partial class Service1
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.Path = "C:\\Users\\Faba Binch\\Documents\\Cours M2 IPI\\C#\\Test_Watcher";
+            // 
+            // Service1
+            // 
+            this.ServiceName = "Service1";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.EventLog eventLog1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+    }
+}
---
+++ MyService/Service1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyService
+{
+    public partial class Service1 : ServiceBase
+    {
+        private string fwPath = "C:\\Users\\Faba Binch\\Documents\\Cours M2 IPI\\C#\\Test_Watcher";
+        private string fileName = "Log.csv";
+        private string filePath = "";
+
+        private string date;
+        private DateTime dateTime;
+
+        private StreamWriter sw;
+        
+
+        public Service1()
+        {
+            InitializeComponent();
+            filePath = fwPath + "\\" + fileName;
+
+            date = dateTime.ToString("dd/MM/yyyy");
+
+            if (!File.Exists(filePath))
+            {
+                string entete = "Date;Category;Code;Message";
+                writeInFile(entete);
+               
+            }
+
+            fileSystemWatcher1.Path = fwPath;
+            fileSystemWatcher1.Renamed += FileSystemWatcher1_Renamed;
+            fileSystemWatcher1.Changed += FileSystemWatcher1_Changed;
+            fileSystemWatcher1.Created += FileSystemWatcher1_Created;
+            fileSystemWatcher1.Deleted += FileSystemWatcher1_Deleted;
+        }
+
+        private void FileSystemWatcher1_Deleted(object sender, System.IO.FileSystemEventArgs e)
+        {
+           string textDeleted = date + ";Deleted;111;Fichier " + e.Name + " supprimé";
+            writeInFile(textDeleted);
+        }
+
+        private void FileSystemWatcher1_Created(object sender, System.IO.FileSystemEventArgs e)
+        {
+            string textCreated = date + ";Created;222;Fichier " + e.Name + "créé";
+            writeInFile(textCreated);
+        }
+
+        private void FileSystemWatcher1_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+            string textChanged = date + ";Changed;333;Fichier " + e.Name +  " modifié (" + e.ChangeType + ")";
+            writeInFile(textChanged);
+        }
+
+        private void FileSystemWatcher1_Renamed(object sender, System.IO.RenamedEventArgs e)
+        {
+            string textRenamed = date + ";Renamed;444;Fichier " + e.OldName + " renomé " + e.Name;
+            writeInFile(textRenamed);
+        }
+
+        private void writeInFile(string text)
+        {
+            using (StreamWriter sw = File.CreateText(filePath))
+            {
+                sw.WriteLine(text);
+               
+            }
+        }
+    }
+}
